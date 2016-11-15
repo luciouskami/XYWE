1462029dd4564af5692620d7516583a068b85b51
LazyKnight <i@lazyknight.com>
2016-11-15 12:14:39 +0800

    Change VersionClass

--- Develop/XYBase/XYInfo.cs
+++ Develop/XYBase/XYInfo.cs
@@ -8,7 +8,7 @@ namespace XYBase {
     public static class XYInfo {
         public const string YDVersion = "1.30.4";
         public const string Version = "1.1.Alpha.7"; // Used in version verify & launcher's version mark and update check
-        public const string VersionClass = "内部预览版 & 预发布版";
+        public const string VersionClass = "内部预览版";
 
         public const string EditorName = "咸鱼地图编辑器";
         public const string EditorNameMin = "XY World Editor";
