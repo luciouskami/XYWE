01e550aaf89c98531cb31031dd46c1078cbb79ba
LazyKnight <i@lazyknight.com>
2016-11-27 04:02:20 +0800

    Notice developer build Develop-Editor if they didn't
    Signed-off-by: LazyKnight <i@lazyknight.com>

--- XYWE.exe Develop.bat
+++ XYWE.exe Develop.bat
@@ -1,1 +1,9 @@
-START "" "XYWE.exe" "Develop"
+@ECHO OFF
+IF EXIST "XYWE.exe" (
+	START "" "XYWE.exe" "Develop"
+) ELSE (
+	ECHO Can't find XYWE.exe at current directory
+	ECHO You should build Develop-Editor first
+	ECHO=
+	PAUSE
+)
