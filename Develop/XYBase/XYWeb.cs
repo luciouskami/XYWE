7e1027147d59ea64dfc29fa86c94b7b5692151e2
LazyKnight <i@lazyknight.com>
2016-11-29 01:20:40 +0800

    Remove debug xywe server address
    At current time, it's better to direct connect server and debug
    
    Signed-off-by: LazyKnight <i@lazyknight.com>

--- Develop/XYBase/XYWeb.cs
+++ Develop/XYBase/XYWeb.cs
@@ -13,11 +13,7 @@ namespace XYBase
 {
     public static class XYWeb
     {
-#if DEBUG
-        static string xyweServerAddress = "http://192.168.1.101/xywe_server";
-#else
         static string xyweServerAddress = "https://wow9.org/xywe_server";
-#endif
 
         public static async void DownloadDataAsync(string address, Action<byte[]> successAction)
         {
